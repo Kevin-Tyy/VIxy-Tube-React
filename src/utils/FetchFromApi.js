f18796d0fd0664303a739041aa4d4fec5830acfe
Kevin Tuyizere <tuyizerek27@gmail.com>
2023-04-21 11:15:55 +0200

    fixing netlify deploy issues

--- src/utils/FetchFromApi.js
+++ src/utils/FetchFromApi.js
@@ -10,7 +10,7 @@ const options = {
         maxResults: '50'
     },
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Key': '4e398042e3msh659ac940dbb6dcap1d7a79jsndddf05ac940b',
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
