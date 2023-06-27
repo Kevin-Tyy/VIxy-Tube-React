251c6e9a5562048a609e757732b4f4c5faffe458
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-06-27 05:32:54 +0200

    Creating a custom skeleton loader for a video card

--- src/Components/Loaders/Skeleton/VideoCard.jsx
+++ src/Components/Loaders/Skeleton/VideoCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Skeleton from '@mui/material'
+import { Skeleton } from "@mui/material";
 
 const VideoCard = () => {
 	const numberOfLoaders = 40;
@@ -8,17 +8,31 @@ const VideoCard = () => {
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 p-4 md:p-12 gap-6">
 			{loaderArray.map((_, index) => (
 				<div
-					className="bg-[#1a1b2b9d] p-4 flex flex-col gap-4 rounded-lg"
+					className="bg-[#1a1b2b5d] p-4 flex flex-col gap-4 rounded-lg"
 					key={index}>
-					<div className="flex w-full gap-4">
-						<Skeleton circle width={100} height={100} />
-						<div className="w-full flex flex-col gap-2">
-							<Skeleton />
-							<Skeleton />
-							<Skeleton height={50} />
+					<div className="flex flex-col w-full gap-2">
+						<div>
+							<Skeleton
+								variant="rectangular"
+								sx={{ bgcolor: "#1a1b2b" }}
+								width={"100%"}
+								height={"200px"}
+							/>
 						</div>
+						<div className="w-full flex items-center justify-center gap-2">
+							<Skeleton
+								variant="rounded"
+								sx={{ bgcolor: "#1a1b2b", borderRadius: "50%" }}
+								width="60px"
+								height="50px"
+							/>
+							<div className="w-full">
+								<Skeleton sx={{ bgcolor: "#1a1b2b" }} />
+								<Skeleton sx={{ bgcolor: "#1a1b2b" }} />
+							</div>
+						</div>
+						<Skeleton sx={{ bgcolor: "#1a1b2b" }} />
 					</div>
-					<Skeleton />
 				</div>
 			))}
 		</div>
