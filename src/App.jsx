e6b905d51c621196ae666ed815df3e275d33ccef
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-06-27 18:30:33 +0200

    Layout customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Navbar, ChannelDetails, VideoDetails, Feed, SearchFeed  } from './Components'
+import {
+	Navbar,
+	ChannelDetails,
+	VideoDetails,
+	Feed,
+	SearchFeed,
+} from "./Components";
 const App = () => (
-    <BrowserRouter>
-        <div className='bg-primary-dark w-full'>
-            <Navbar/>
-            <Routes>
-                
-                <Route path='/' exact element={<Feed/>}/>
-                <Route path='/video/:id' element={<VideoDetails/>} />
-                <Route path='/channel/:id' element={<ChannelDetails/>} />
-                <Route path='/search/:searchTerm' element={<SearchFeed/>} />
-            </Routes>
-        </div> 
-    </BrowserRouter>
-)
-export default App
+	<BrowserRouter>
+		<div className="min-h-screen bg-primary-dark w-full">
+			<Navbar />
+			<Routes>
+				<Route path="/" exact element={<Feed />} />
+				<Route path="/video/:id" element={<VideoDetails />} />
+				<Route path="/channel/:id" element={<ChannelDetails />} />
+				<Route path="/search/:searchTerm" element={<SearchFeed />} />
+			</Routes>
+		</div>
+	</BrowserRouter>
+);
+export default App;
